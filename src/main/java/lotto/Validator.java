60c11301d5fc393f54ffa1a44843653406623513
2021037156 <tmj5574@hanyang.ac.kr>
2025-10-31 19:00:54 +0900

    chore: 구매 입력값 검증 로직 수정

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,13 +1,10 @@
 package lotto;
 
 public class Validator {
-    private static final String ERROR_BELOW_ZERO = "[ERROR] 구매 금액은 음수일 수 없습니다.";
-    private static final String ERROR_NOT_1000_UNIT = "ERROR] 구매 금액은 0 초과의 1000원의 배수여야 합니다.";
+
+    private static final String ERROR_NOT_1000_UNIT = "ERROR] 구매 금액은 0원 초과 및 1000원의 배수여야 합니다.";
 
     public void validatePurchaseAmount(int purchaseAmount) {
-        if (purchaseAmount < 0) {
-            throw new IllegalArgumentException(ERROR_BELOW_ZERO);
-        }
         if (purchaseAmount % 1000 != 0 || purchaseAmount == 0) {
             throw new IllegalArgumentException(ERROR_NOT_1000_UNIT);
         }
