3cc465e5a37f3a4a70fa1a0beba21c4f3bbd0bf0
2021037156 <tmj5574@hanyang.ac.kr>
2025-10-31 20:47:09 +0900

    test: 사용자 입력 보너스 번호 테스트 추가

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
@@ -32,7 +33,7 @@ public class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    private static Stream<Arguments> paramsForPurchaseInvalidInputs () {
+    private static Stream<Arguments> paramsForPurchaseInvalidInputs() {
         return Stream.of(
                 Arguments.of("0"),
                 Arguments.of("-5000"),
@@ -41,17 +42,34 @@ public class ValidatorTest {
     }
 
     @ParameterizedTest
-    @MethodSource("paramsForPurchaseInvalidInputs")
+    @MethodSource("paramsForUserLottoInputs")
     void 입력한_로또_번호가_올바르지_않은_경우_예외가_발생한다(String userLottoInputs) {
         assertThatThrownBy(() -> validator.validatePurchaseAmount(userLottoInputs))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    private static Stream<Arguments> paramsForUserLottoInputs () {
+    private static Stream<Arguments> paramsForUserLottoInputs() {
         return Stream.of(
                 Arguments.of("1,2,3,4"),
                 Arguments.of("a,b,c"),
                 Arguments.of("0,1,3,5,46")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramsForUserBonusInputs")
+    void 입력한_보너스_번호가_올바르지_않은_경우_예외가_발생한다(String userBonusInputs, List<Integer> userLottoNumbers) {
+        assertThatThrownBy(() -> validator.validateBonusNumber(userBonusInputs, userLottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> paramsForUserBonusInputs() {
+        List<Integer> defaultWinningNumbers = List.of(1, 2, 3, 4, 5, 8);
+        return Stream.of(
+                Arguments.of("-1", defaultWinningNumbers),
+                Arguments.of("50", defaultWinningNumbers),
+                Arguments.of("-", defaultWinningNumbers),
+                Arguments.of("-6", List.of(1,2,4,5,6,7))
         );
     }
 
