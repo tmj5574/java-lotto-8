373d1ab8f8f0b46ede14e05f8b3819d0dee12e6f
2021037156 <tmj5574@hanyang.ac.kr>
2025-11-03 15:36:33 +0900

    test: 일치하는 번호 개수 확인 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,4 +23,17 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("입력한 당첨 번호와 로또 번호를 비교하여 일치하는 개수를 반환한다.")
+    void 일치하는_로또_번호_개수_확인(){
+        //given
+        Lotto myLotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto winningLotto = new Lotto(List.of(1,2,3,7,8,9));
+
+        //when
+        int matchCount = myLotto.countMatchingNumbers(winningLotto);
+
+        //then
+        assertThat(matchCount).isEqualTo(3);
+    }
 }
